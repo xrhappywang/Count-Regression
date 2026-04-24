65684c39076c0c7ee374b1803312830f8616d7db
Xinran Wang <xwang5@colgate.edu>
2026-04-24 11:49:19 -0400

    Add if/else branches for all count model types

--- smartcount/R/fit.R
+++ smartcount/R/fit.R
@@ -6,6 +6,28 @@
 #'
 #' @return fitted model
 #' @export
-fit_count <- function(data, formula, model = "poisson") {
-  stats::glm(formula, data = data, family = poisson(link = "log"))
+fit_count <- function(data, formula, model) {
+  model <- tolower(model)
+  if (model=="poisson"){
+    fit<-stats::glm(formula, data = data, family = poisson(link = "log"))
+  }
+
+  elseif(model=="quasipoisson"){
+    fit <- stats::glm(formula, data=data, family=quasipoisson(link="log"))
+  }
+
+  elseif(model=="negbin"){
+    fit <- MASS::glm.nb(formula, data=data)
+  }
+  elseif(model=="zip"){
+    fit <- pscl::zeroinfl(formula, data=data, dist="poisson")
+  }
+  elseif(model=="zinb"){
+    fit <= pscl::zeroinfl(formula, data=data, dist="negbin")
+  }
+  else{
+    stop("Model is not supported. Choose: poisson, quasipoisson, negbin, zip, zinb")
+  }
+  
 }
+
